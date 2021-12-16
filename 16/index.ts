f4370c0dba582be4812e762d96d7a0a6b20ff481
Thies Nieborg <thies.nieborg@student.uva.nl>
2021-12-16 14:44:13 +0100

    yeet

--- 16/index.ts
+++ 16/index.ts
@@ -33,25 +33,106 @@ const part1 = (input: string) => {
 		}
 	}
 	result = versionNumberSum;
-	// let data_parsed = data.map((x) => {
-	// 	let packet: { version?: number; type?: number } = {};
-	// 	packet.version = parseInt(x.substr(0, 3), 2);
-	// 	packet.type = parseInt(x.substr(3, 3), 2);
-
-	// 	if (x[6] == "1") {
-	// 		console.log("Number of subpackets conatined");
-	// 	} else {
-	// 		console.log("Lengthof subpackets");
-	// 	}
-	// });
-
-	// console.log(data_parsed);
-
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
 
 	return result;
 };
+
+type packet = {
+	version: number;
+	type: number;
+	value?: number;
+	subPackets?: packet[];
+};
+
+function constructPacket(data: string): { packet: packet; rest: string } {
+	const chars = data.split("");
+
+	const version = parseInt(chars.splice(0, 3).join(""), 2);
+	const type = parseInt(chars.splice(0, 3).join(""), 2);
+
+	if (type == 4) {
+		let literalBinary = "";
+
+		while (true) {
+			const sub = chars.splice(0, 5).join("").padEnd(5, "0");
+			literalBinary += sub.substring(1);
+
+			if (sub[0] === "0") break;
+		}
+
+		return {
+			packet: { version, type, value: parseInt(literalBinary, 2) },
+			rest: chars.join(""),
+		};
+	}
+
+	const lengthType = chars.splice(0, 1)[0];
+
+	if (lengthType == "0") {
+		// 15
+		let totalLength = parseInt(chars.splice(0, 15).join(""), 2);
+		let subPacketContent = chars.splice(0, totalLength).join("");
+
+		let newPackets: packet[] = [];
+		while (subPacketContent.length > 0) {
+			const { packet, rest } = constructPacket(subPacketContent);
+
+			newPackets.push(packet);
+			subPacketContent = rest;
+		}
+
+		return {
+			packet: { version, type, subPackets: newPackets },
+			rest: chars.join(""),
+		};
+	} else {
+		const subPacketCount = parseInt(chars.splice(0, 11).join(""), 2);
+		let subData = chars.join("");
+
+		const subPackets: packet[] = [];
+
+		for (let i = 0; i < subPacketCount; i++) {
+			const { packet, rest } = constructPacket(subData);
+
+			subPackets.push(packet);
+			subData = rest;
+		}
+
+		return {
+			packet: { version, type, subPackets },
+			rest: subData,
+		}; //11
+	}
+}
+
+function getPacketValue(packet: packet): number {
+	if (packet.type == 4) return (packet as { value: number }).value;
+
+	const subPacketValues = (packet as { subPackets: packet[] }).subPackets.map(
+		(subPacket) => getPacketValue(subPacket)
+	);
+
+	switch (packet.type) {
+		case 0:
+			return subPacketValues.reduce((a, b) => a + b);
+		case 1:
+			return subPacketValues.reduce((a, b) => a * b);
+		case 2:
+			return Math.min(...subPacketValues);
+		case 3:
+			return Math.max(...subPacketValues);
+		case 5:
+			return subPacketValues[0] > subPacketValues[1] ? 1 : 0;
+		case 6:
+			return subPacketValues[0] < subPacketValues[1] ? 1 : 0;
+		case 7:
+			return subPacketValues[0] === subPacketValues[1] ? 1 : 0;
+		default:
+			throw new Error("Invalid packet type: " + packet.type);
+	}
+}
 
 // Part 2
 // ======
@@ -61,7 +142,14 @@ const part2 = (input: string) => {
 	const start = now();
 	let result = 0;
 
-	const data = input.split("\n");
+	let data = BigInt(`0x${input}`)
+		.toString(2)
+		// input.length hex chars that are 4 bits each
+		.padStart(4 * input.length, "0");
+
+	const rootPacket = constructPacket(data).packet;
+
+	result = getPacketValue(rootPacket);
 
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));

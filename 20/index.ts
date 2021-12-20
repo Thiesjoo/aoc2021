2a01eb9e77e6180df8421a1af7149cd42af0e634
Thies Nieborg <thies2003@gmail.com>
2021-12-20 12:14:23 +0100

    😚 working now (:

--- 20/index.ts
+++ 20/index.ts
@@ -1,9 +1,12 @@
 import { default as now } from "performance-now";
 
+type Image = boolean[][];
+
 function neighbors(
-	map: { [key: string]: boolean },
+	map: Image,
 	i: number,
-	j: number
+	j: number,
+	enhanceAlternative: boolean
 ): boolean[] {
 	const directions = [
 		[-1, -1],
@@ -18,100 +21,61 @@ function neighbors(
 	];
 
 	return directions.map((x) => {
-		return get(map, i + x[0], j + x[1]);
+		return get(map, i + x[0], j + x[1], enhanceAlternative);
 	});
 }
 
-function findLargest(map: { [key: string]: boolean }) {
-	let temp = Object.keys(map).map((x) => x.split(",").map(Number));
-
-	let maxX = Math.max(...temp.map((x) => x[0])) + 1;
-	let minX = Math.min(...temp.map((x) => x[0])) - 1;
-
-	let maxY = Math.max(...temp.map((x) => x[1])) + 1;
-	let minY = Math.min(...temp.map((x) => x[1])) - 1;
-
-	return {
-		i: { maxX, minX },
-		j: { maxY, minY },
-	};
+function get(
+	map: Image,
+	i: number,
+	j: number,
+	enhanceAlternative: boolean
+): boolean {
+	return map[i]?.[j] ?? enhanceAlternative;
 }
 
-function get(map: { [key: string]: boolean }, i: number, j: number): boolean {
-	return map[`${i},${j}`] || false;
+function parseInput(input: string, part2: boolean): number {
+	const [enhance2, image2] = input.split("\n\n");
+	let image = image2.split("\n").map((x) => x.split("").map((x) => x === "#"));
+
+	const enhance = enhance2.split("").map((x) => x === "#");
+
+	return simulate(image, enhance, part2 ? 50 : 2);
+}
+
+function simulate(
+	image: Image,
+	enhance: boolean[],
+	totalTimes: number
+): number {
+	for (let times = 0; times < totalTimes; times++) {
+		const nextImg: Image = [];
+		for (let i = -1; i < image.length + 1; i++) {
+			nextImg.push([]);
+
+			for (let j = -1; j < image.length + 1; j++) {
+				const neigh = neighbors(image, i, j, enhance[0] && times % 2 == 1);
+				const enhanceIndex = parseInt(
+					neigh.map((x) => (x ? "1" : "0")).join(""),
+					2
+				);
+
+				nextImg[nextImg.length - 1].push(enhance[enhanceIndex]);
+			}
+		}
+		image = nextImg;
+	}
+
+	return image.flat().filter(Boolean).length;
 }
 
 // Part 1
 // ======
-// ~0 ms - answer: 0
+// ~48 ms - answer: 5619
 
 const part1 = (input: string) => {
 	const start = now();
-	let result = 0;
-
-	const [enhance2, image2] = input.split("\n\n");
-	let image = image2.split("\n").map((x) => x.split(""));
-
-	let map: { [key: string]: boolean } = {};
-
-	image.forEach((x, i) => {
-		x.forEach((y, j) => {
-			map[`${i},${j}`] = y == "#";
-		});
-	});
-
-	let newMap: { [key: string]: boolean } = {};
-	let max = findLargest(map);
-
-	for (let i = max.i.minX; i < max.i.maxX; i++) {
-		console.log(
-			Object.entries(map)
-				.filter((x) => x[0].split(",").map(Number)[0] == i)
-				.map((x) => (x[1] ? "#" : "."))
-				.join("")
-		);
-	}
-
-	for (let i = max.i.minX; i < max.i.maxX; i++) {
-		for (let j = max.j.minY; j < max.j.maxY; j++) {
-			let neigh = neighbors(map, i, j);
-			let newest = parseInt(neigh.map((x) => (x ? "1" : "0")).join(""), 2);
-			newMap[`${i},${j}`] = enhance2[newest] == "#";
-		}
-	}
-
-	for (let i = max.i.minX; i < max.i.maxX; i++) {
-		console.log(
-			Object.entries(newMap)
-				.filter((x) => x[0].split(",").map(Number)[0] == i)
-				.map((x) => (x[1] ? "#" : "."))
-				.join("")
-		);
-	}
-
-	map = newMap;
-	max = findLargest(map);
-
-	newMap = {};
-
-	for (let i = max.i.minX; i < max.i.maxX; i++) {
-		for (let j = max.j.minY; j < max.j.maxY; j++) {
-			let neigh = neighbors(map, i, j);
-			let newest = parseInt(neigh.map((x) => (x ? "1" : "0")).join(""), 2);
-			newMap[`${i},${j}`] = enhance2[newest] == "#";
-		}
-	}
-
-	for (let i = max.i.minX; i < max.i.maxX; i++) {
-		console.log(
-			Object.entries(newMap)
-				.filter((x) => x[0].split(",").map(Number)[0] == i)
-				.map((x) => (x[1] ? "#" : "."))
-				.join("")
-		);
-	}
-
-	console.log(Object.entries(newMap).filter((x) => x[1]).length);
+	let result = parseInput(input, false);
 
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
@@ -121,13 +85,11 @@ const part1 = (input: string) => {
 
 // Part 2
 // ======
-// ~0 ms - answer: 0
+// ~900 ms - answer: 20122
 
 const part2 = (input: string) => {
 	const start = now();
-	let result = 0;
-
-	const data = input.split("\n");
+	let result = parseInput(input, false);
 
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
